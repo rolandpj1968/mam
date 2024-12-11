9bfb8a688f4af0c1633fe3e0010877a59fce3595
Roland Paterson-Jones <rolandpj@gmail.com>
2024-12-11 11:46:41 +0200

    aop stack impact; need to separate a0, a1 types

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
 int main() {
 	printf("\n%d ALU ops\n\n", NAOp);
 	for (int op = 0; op < NAOp; op++) {
+		if ((op&0x7) == 0)
+			printf("--------\n");
 		printf("    %4d 0x%02x %3s %10s %3s * %d -> %3s * %d\n", op, op, UNITS[aoptab[op].o.unit], aoptab[op].o.name, TYPES[aoptab[op].ta], aoptab[op].na, TYPES[aoptab[op].tr], aoptab[op].nr);
 	}
 }
